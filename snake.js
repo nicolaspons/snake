e9230450a63ab9be02eb64269e7d29bfb05ff1c3
Nicolas Pons <nicolas.pons96@gmail.com>
2018-12-21 17:54:49 +0100

    snake.js: last pos is still displayed even after the reset

--- snake.js
+++ snake.js
@@ -1,37 +1,32 @@
 const canvas = document.getElementById('snake');
 const context = canvas.getContext('2d');
-context.scale(20, 20);
+const scale = 10;
+const size = canvas.width / scale
+context.scale(scale, scale);
 
-function arenaSweep() {
-    let rowCount = 1;
-    outer: for (let y = arena.length - 1; y > 0; --y) {
-        for (let x = 0; x < arena[y].length; ++x) {
-            if (arena[y][x] === 0) {
-                continue outer;
-            }
-        }
-        const row = arena.splice(y, 1)[0].fill(0);
-        arena.unshift(row);
-        ++y;
-
-        player.score += rowCount * 10;
-        rowCount *= 2;
-    }
+const arena = createMatrix(size, size);
+const player = {
+    pos: { x: 5, y: 5 },
+    prev: { x: 5, y: 5 },
+    next: { x: 0, y: -1 },
+    matrix: [],
+    score: 0
 }
 
-function collide(arena, player) {
-    const [m, o] = [player.matrix, player.pos];
-    for (let y = 0; y < m.length; ++y) {
-        for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0 && (arena[y + o.y] &&
-                    arena[y + o.y][x + o.x]) !== 0) {
-                return true;
-            }
-        }
-    }
-    return false;
+/**
+ * check if the player hits the border
+ * @param {*} arena 
+ * @param {*} player 
+ */
+function collide() {
+    return player.pos.x < 0 || player.pos.y < 0 || player.pos.x >= size || player.pos.y >= size;
 }
 
+/**
+ * Create the map
+ * @param {width} w 
+ * @param {height} h 
+ */
 function createMatrix(w, h) {
     const matrix = [];
     while (h--) {
@@ -40,149 +35,40 @@ function createMatrix(w, h) {
     return matrix;
 }
 
-function createPiece(type) {
-    if (type === 'T') {
-        return [
-            [0, 0, 0],
-            [1, 1, 1],
-            [0, 1, 0]
-        ];
-    } else if (type === 'O') {
-        return [
-            [2, 2],
-            [2, 2]
-        ];
-    } else if (type === 'L') {
-        return [
-            [0, 3, 0],
-            [0, 3, 0],
-            [0, 3, 3]
-        ];
-    } else if (type === 'J') {
-        return [
-            [0, 4, 0],
-            [0, 4, 0],
-            [4, 4, 0]
-        ];
-    } else if (type === 'I') {
-        return [
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-            [0, 5, 0, 0]
-        ];
-    } else if (type === 'S') {
-        return [
-            [0, 6, 6],
-            [6, 6, 0],
-            [0, 0, 0]
-        ];
-    } else if (type === 'Z') {
-        return [
-            [7, 7, 0],
-            [0, 7, 7],
-            [0, 0, 0]
-        ];
-    }
-}
-
 function draw() {
+    context.fillStyle = '#fff';
+    context.fillRect(player.prev.x, player.prev.y, 1, 1);
+    context.fillRect(player.pos.x, player.pos.y, 1, 1);
     context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    drawMatrix(arena, { x: 0, y: 0 });
-    drawMatrix(player.matrix, player.pos);
+    context.fillRect(player.pos.x + 0.25, player.pos.y + 0.25, 0.5, 0.5);
 }
 
-function drawMatrix(matrix, offset) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                context.fillStyle = colors[value];
-                context.fillRect(x + offset.x, y + offset.y, 1, 1);
-            }
-        });
-    });
-}
-
-function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
-            }
-        });
-    });
-}
-
-function playerDrop() {
-    player.pos.y++;
-    if (collide(arena, player)) {
-        player.pos.y--;
-        merge(arena, player);
+function playerMove() {
+    if (collide(arena)) {
         playerReset();
-        arenaSweep();
-        updateScore();
-    }
-    dropCounter = 0;
-}
+    } else {
+        player.prev.x = player.pos.x;
+        player.prev.y = player.pos.y;
+        player.pos.x += player.next.x;
+        player.pos.y += player.next.y;
 
-function playerMove(dir) {
-    player.pos.x += dir;
-    if (collide(arena, player)) {
-        player.pos.x -= dir;
+        console.log(player.prev.x + "," + player.prev.y + " // " + player.pos.x + "," + player.pos.y);
     }
+    updateScore();
 }
 
 function playerReset() {
-    const pieces = 'ILJOTSZ';
-    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
-    player.pos.y = 0;
-    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
-    if (collide(arena, player)) {
-        arena.forEach(row => row.fill(0));
-        player.score = 0;
-        updateScore();
-    }
-}
+    player.pos = { x: 5, y: 5 };
+    player.score = 0;
+    player.body = [];
+    player.body.push(player.pos);
 
-function playerRotate(dir) {
-    const pos = player.pos.x;
-    let offset = 1;
-    rotate(player.matrix, dir);
-    while (collide(arena, player)) {
-        player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length) {
-            rotate(player.matrix, -dir);
-            player.pos.x = pos;
-            return;
-        }
-    }
-}
-
-//rotate = transpose + reverse
-function rotate(matrix, dir) {
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < y; ++x) {
-            [
-                matrix[x][y],
-                matrix[y][x]
-            ] = [
-                matrix[y][x],
-                matrix[x][y]
-            ];
-        }
-    }
-    if (dir > 0) {
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse;
-    }
+    context.fillStyle = '#202028';
+    context.fillRect(0, 0, size, size);
 }
 
 let dropCounter = 0;
-let dropInterval = 1000;
+let dropInterval = 250;
 let lastTime = 0;
 
 function update(time = 0) {
@@ -190,7 +76,8 @@ function update(time = 0) {
     lastTime = time;
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
-        playerDrop();
+        playerMove();
+        dropCounter = 0;
     }
     draw();
     requestAnimationFrame(update);
@@ -200,38 +87,16 @@ function updateScore() {
     document.getElementById('score').innerText = player.score;
 }
 
-const colors = [
-    null,
-    '#FF0D72',
-    '#0DC2FF',
-    '#0DFF72',
-    '#F538FF',
-    '#FF8E0D',
-    '#FFE138',
-    '#3877FF'
-];
-
-const arena = createMatrix(12, 20);
-
-const player = {
-    pos: { x: 0, y: 0 },
-    matrix: null,
-    score: 0
-}
-
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
-        playerMove(-1);
+        player.next = { x: -1, y: 0 };
+    } else if (event.keyCode === 38) {
+        player.next = { x: 0, y: -1 };
     } else if (event.keyCode === 39) {
-        playerMove(1);
+        player.next = { x: 1, y: 0 };
     } else if (event.keyCode === 40) {
-        playerDrop();
-    } else if (event.keyCode === 81) {
-        playerRotate(-1);
-    } else if (event.keyCode === 87) {
-        playerRotate(1);
+        player.next = { x: 0, y: 1 };
     }
-
 })
 
 playerReset();
