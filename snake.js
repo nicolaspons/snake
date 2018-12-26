349c22a25a99fbae3b5d310daa2276fb22d1d9bc
Nicolas Pons <nicolas.pons96@gmail.com>
2018-12-26 10:17:29 +0100

    snake.js: Z Q S D added

--- snake.js
+++ snake.js
@@ -147,13 +147,14 @@ function updateScore() {
 }
 
 document.addEventListener('keydown', event => {
-    if (event.keyCode === 37) {
+    if (event.keyCode === 37 || event.keyCode === 81) {
         player.next = { x: -1, y: 0 };
-    } else if (event.keyCode === 38) {
+    } else if (event.keyCode === 38 || event.keyCode === 90) {
         player.next = { x: 0, y: -1 };
-    } else if (event.keyCode === 39) {
+    } else if (event.keyCode === 39 || event.keyCode === 68) {
+        console.log('tamer');
         player.next = { x: 1, y: 0 };
-    } else if (event.keyCode === 40) {
+    } else if (event.keyCode === 40 || event.keyCode === 83) {
         player.next = { x: 0, y: 1 };
     }
 })
